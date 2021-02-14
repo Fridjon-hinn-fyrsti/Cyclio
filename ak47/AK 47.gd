a546e137d2a1a7d2929dced474efa25f95a09916
Fridjon-hinn-fyrsti <gudmundurfe19@ru.is>
2021-02-14 18:18:43 +0000

    skokk hreyfing og apar

---
+++ ak47/AK 47.gd
@@ -0,0 +1,58 @@
+extends Spatial
+
+var x_rot = rotation.x
+
+onready var gunfire = get_node("Fire_gun")
+onready var emptyclip = get_node("Empty_clip")
+onready var reloadsound = get_node("Reload")
+onready var animator = get_node("AnimationPlayer")
+onready var raycast = get_node("Model/RayCast")
+onready var bullet_counter = get_node("Bullet_counter")
+
+var bullets = 30
+
+func shoot():
+	if not reloadsound.playing:
+		if bullets > 0:
+			if not animator.is_playing():
+				bullets -= 1
+				update_counter()
+				gunfire.play()
+				if raycast.is_colliding():
+					var target = raycast.get_collider()
+					if target.is_in_group("Enemies"):
+						target.hit(raycast.get_collision_normal())
+				animator.play("Fire_gun")
+		else:
+			if not emptyclip.playing:
+				emptyclip.play()
+
+func update_counter():
+	bullet_counter.text = "%s/30" % bullets
+
+func reload():
+	if not reloadsound.playing:
+		animator.play("Reload_gun")
+		reloadsound.play()
+		bullets = 30
+		update_counter()
+
+func reset_rotation():
+	rotation.x = x_rot
+		
+		
+func _input(event):
+	if event is InputEventMouseMotion and get_node("FPS_camera").current:
+		var movement = event.relative
+		var sensitivity = 0.1
+		rotation.x -= deg2rad(movement.y * sensitivity) #*cos(rotation.z)
+		#rotation.y -= deg2rad(movement.x * sensitivity)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
