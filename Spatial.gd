7f2f94b2d8c4647c2d2392bb8ef9872f9382ee7c
Fridjon-hinn-fyrsti <gudmundurfe19@ru.is>
2021-02-14 21:59:55 +0000

    monkey ded

--- Spatial.gd
+++ Spatial.gd
@@ -7,8 +7,35 @@ extends Spatial
 
 
 # Called when the node enters the scene tree for the first time.
+var trees = [preload("res://Trees/Tree1.tscn"),preload("res://Trees/Tree2.tscn"),preload("res://Trees/Tree3.tscn"),preload("res://Trees/Tree4.tscn"),preload("res://Trees/Tree5.tscn"),preload("res://Trees/Tree6.tscn")]
+onready var planet = get_node("Planet")
+var rng = RandomNumberGenerator.new()
+onready var R = 100
+
+
+func fix_angle(trans):
+	var planet_vector = (trans.origin - planet.transform.origin).normalized()
+	trans.basis.y = planet_vector
+	trans.basis.z = trans.basis.y.rotated(trans.basis.x, deg2rad(90))
+	trans.basis.x = trans.basis.y.cross(trans.basis.z)
+	trans = trans.orthonormalized()
+	return trans
+
 func _ready():
-	pass
+	rng.randomize()
+	for i in range(200):
+		var theta1 = rng.randf_range(0, 2*PI)
+		var theta2 = rng.randf_range(0, PI)
+		var x = cos(theta1)*sin(theta2)*R
+		var z = sin(theta1)*sin(theta2)*R
+		var y = cos(theta2)*R
+		var obj = trees[rng.randi()%len(trees)].instance()
+		obj.translation = Vector3(x,y,z)
+		obj.transform = fix_angle(obj.transform)
+		var trans = obj.transform.basis
+		var scale = 10
+		obj.transform.basis = Basis(trans.x*scale, trans.y*scale, trans.z*scale)
+		add_child(obj)
 	#spawn evil bot
 	var evil_bot = preload("res://Evil_bot/Evil_bot.tscn")
 	for i in range(10):
@@ -18,9 +45,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-onready var planet = get_node("Planet")
 func _process(delta):
 	for child in get_children():
 		if child is KinematicBody:
 			var vec = (planet.translation - child.translation).normalized()
-			child.move_and_slide(vec)
+			child.move_and_slide(vec*delta*100)
+		if child is RigidBody:
+			var vec = (planet.translation - child.translation).normalized()
+			child.add_central_force(vec*delta*1000)
